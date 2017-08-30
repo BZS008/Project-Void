32fc56f6293620f5117cb6032278f1cd2daf5455
Daniel <danielcox16@hotmail.com>
2017-08-30 22:18:35 +0200

    Add Level Editor Save
    Save level to file with F key. This will generate the level tile data as javascript code. At the moment only some basic metadata and the tile type indices are saved.

--- js/leveleditor.js
+++ js/leveleditor.js
@@ -50,9 +50,60 @@ var leveleditor = {
             } else if (!this.tileswitch && keydown.c) {
                 this.tileindex = tid;       // Copy tiletype from under mouse
                 this.tileswitch = true;     // Stop switching until no key is pressed
+            
+            // F - Save level data
+            } else if (!this.tileswitch && keydown.f) {
+                
+                
+                //--- Generate level code ---//
+                var lvlcode = `
+// Level Name
+level.name = "` + level.name + `";
+
+// Tile size
+level.tilewidth = ` + level.tilewidth + `;
+level.tileheight = ` + level.tileheight + `;
+
+// Tileset
+level.tileset = "` + level.tileset + `";
+
+// Level Data
+level.data=[\n[`;
+                
+                // Loop over tiles for saving
+                var ni = level.data.length;
+                for (var i = 0; i < ni; i++) {          // Loop over i
+                    var nj = level.data[i].length;
+                    
+                    for (var j = 0; j < nj; j++) {      // Loop over j
+                        var tid = level.data[i][j];     // Get tile type
+                        lvlcode += tid;
+                        
+                        // Insert appropriate delimiter
+                        if (j == nj-1) {
+                            if (i == ni-1) {            // Last col last row
+                                lvlcode += ']];';
+                            } else {                    // Just last col
+                                lvlcode += '],\n[';
+                            }
+                        } else {
+                            lvlcode += ',';             // Not at end of row
+                        }
+                    }
+                }
+                //--- Code generation done ---//
+                
+                // Open data as textfile and initiate download
+                var hiddenElement = document.createElement('a');
+                hiddenElement.href = 'data:attachment/text,' + encodeURI(lvlcode);
+                hiddenElement.target = '_blank';
+                hiddenElement.download = 'lvl.js';
+                hiddenElement.click();
+                
+                this.tileswitch = true;     // Stop switching until no key is pressed
                 
             // Block further actions until all keys are released
-        } else if (!(keydown.x || keydown.z || keydown.c)) {
+        } else if (!(keydown.x || keydown.z || keydown.c || keydown.f)) {
                 this.tileswitch = false;
             }
         }
